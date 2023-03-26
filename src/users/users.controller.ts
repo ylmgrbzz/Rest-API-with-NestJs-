3284e1cc783a5e16e125704ba658aabd82e5c76e
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-26 23:54:14 +0300

    Rest API

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { Controller, Get, Param, Put } from "@nestjs/common";
 import { UsersService } from "./users.service";
 import { CreateUsersDto } from "./dto/create-users.dto";
 import { Post, Body } from "@nestjs/common";
@@ -16,7 +16,13 @@ export class UsersController {
 
     @Post()
     create(@Body() userDto: CreateUsersDto) {
-        return `Creating a user ${userDto.name}`
+        return this.usersService.create(userDto);
     }
+
+    @Put(":id")
+    update(@Body() id, @Body, userDto: CreateUsersDto) {
+        return this.usersService.update(id, userDto);
+    }
+
 }
 
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,21 +1,35 @@
 import { Injectable } from "@nestjs/common";
-
+import { InjectModel } from "@nestjs/common";
+import { User, UserDocument } from "./schemas/users.schema";
+import { Model } from "mongoose";
 @Injectable()
 export class UsersService {
-    private readonly users = [
-        {
-            id: "1",
-            name: "John Doe",
-            age: 25
-        },
-        {
-            id: "2",
-            name: "Jane Doe",
-            age: 25
-        }
-    ];
+    // private readonly users = [
+    //     {
+    //         id: "1",
+    //         name: "John Doe",
+    //         age: 25
+    //     },
+    //     {
+    //         id: "2",
+    //         name: "Jane Doe",
+    //         age: 25
+    //     }
+    // ];
+    constructors(@InjectModel(User.name) private userModel: Model<UserDocument>) { }
 
-    findAll() {
-        return this.users;
+    async findAll() {
+        return await this.userModel.find();
     }
+
+    async create(userDto) {
+        const user = new this.userModel(userDto);
+        return await user.save();
+    }
+
+    async update(id, userDto) {
+        return await this.userModel.findByIdAndUpdate(id, userDto, { new: true });
+    }
+
+
 }
