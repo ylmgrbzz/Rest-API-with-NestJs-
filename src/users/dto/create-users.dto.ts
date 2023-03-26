009ac9b8593eba65e6d578c1a11623f1ba4cddb3
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-26 23:09:21 +0300

    Rest API

---
+++ src/users/dto/create-users.dto.ts
@@ -0,0 +1,4 @@
+export class CreateUsersDto {
+    username: string;
+    age: number;
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Param } from "@nestjs/common";
 
 @Controller('users')
 export class UsersController {
@@ -6,5 +6,16 @@ export class UsersController {
     getAll() {
         return "All users";
     }
+    @Get(":id")
+    getOne(@Param('id') id) {
+        return `User ${id}`;
+    }
+
+    @Post()
+    create(@Body() userDto: CreateUsersDto) {
+        return `Creating a user ${userDto.name}`
+    }
+
+
 }
 
