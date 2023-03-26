06c99635ee151e25222f24ffed840baebdbffce4
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-03-27 00:10:43 +0300

    Rest API

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,6 +2,7 @@ import { Injectable, remove } from '@nestjs/common';
 import { InjectModel } from "@nestjs/common";
 import { User, UserDocument } from "./schemas/users.schema";
 import { Model } from "mongoose";
+import { CreateUsersDto } from './dto/create-users.dto';
 @Injectable()
 export class UsersService {
     // private readonly users = [
@@ -22,21 +23,21 @@ export class UsersService {
         return await this.userModel.find();
     }
 
-    async getOne(id): Promise<User> {
+    async getOne(id: string): Promise<User> {
         return await this.userModel.findById(id);
     }
 
 
-    async create(userDto): Promise<User> {
+    async create(userDto: CreateUsersDto): Promise<User> {
         const user = new this.userModel(userDto);
         return await user.save();
     }
 
-    async update(id, userDto): Promise<User> {
+    async update(id: string, userDto: CreateUsersDto): Promise<User> {
         return await this.userModel.findByIdAndUpdate(id, userDto, { new: true });
     }
 
-    async remove(id): Promise<User> {
+    async remove(id: string): Promise<User> {
         return await this.userModel.findByIdAndDelete(id);
     }
 
